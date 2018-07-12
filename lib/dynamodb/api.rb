ced05d759c8555eea6f8ce7d7885c20b56c85d50
WalkerSumida <walkersumida@gmail.com>
2018-07-12 10:41:22 +0900

    first commit

---
+++ lib/dynamodb/api.rb
@@ -0,0 +1,7 @@
+require "dynamodb/api/version"
+
+module Dynamodb
+  module Api
+    # Your code goes here...
+  end
+end
---
+++ spec/dynamodb/api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dynamodb::Api do
+  it "has a version number" do
+    expect(Dynamodb::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
