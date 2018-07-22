f6381df0ddd64b00492288065fef22adb510d78c
WalkerSumida <walkersumida@gmail.com>
2018-07-22 11:31:20 +0900

    feat: add client to query class

--- lib/dynamodb/api/query.rb
+++ lib/dynamodb/api/query.rb
@@ -3,10 +3,12 @@
 module Dynamodb
   module Api
     class Query # :nodoc:
+      attr_reader :client
       attr_reader :relation
 
       def initialize
         @relation = Relation.new
+        @client = Adapter.new.client
       end
     end
   end
