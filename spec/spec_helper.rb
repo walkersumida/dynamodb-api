4ccf0eaaf61385804ba639557e4d4a522bc3c585
WalkerSumida <walkersumida@gmail.com>
2018-07-27 11:38:13 +0900

    chore: set rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,22 @@
-require "bundler/setup"
-require "dynamodb/api"
+require 'bundler/setup'
+require 'dynamodb/api'
+require 'pry'
+
+ENV['ACCESS_KEY'] ||= 'abcd'
+ENV['SECRET_KEY'] ||= '1234'
+
+Aws.config.update(
+  region: 'ap-northeast-1',
+  credentials: Aws::Credentials.new(ENV['ACCESS_KEY'], ENV['SECRET_KEY'])
+)
+
+Dynamodb::Api.config do |config|
+  config.endpoint = 'http://0.0.0.0:8000'
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
