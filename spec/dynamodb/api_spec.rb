46e0d35782c609bf111343a24beefa140fb3e329
WalkerSumida <walkersumida@gmail.com>
2018-07-27 11:40:48 +0900

    test: api spec

--- spec/dynamodb/api_spec.rb
+++ spec/dynamodb/api_spec.rb
@@ -1,9 +1,31 @@
 RSpec.describe Dynamodb::Api do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Dynamodb::Api::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#config' do
+    context 'when block_given is true' do
+      before do
+        Dynamodb::Api.config do |config|
+          config.endpoint = 'http://1.1.1.1:8000'
+        end
+      end
+
+      it 'works' do
+        expect(Dynamodb::Api::Config.endpoint).to eq('http://1.1.1.1:8000')
+      end
+    end
+
+    context 'when block_given is false' do
+      it 'works' do
+        expect(Dynamodb::Api.config).to eq(Dynamodb::Api::Config)
+      end
+    end
+  end
+
+  describe '#adapter' do
+    it 'returns a Dynamodb::Api::Adapter class' do
+      expect(Dynamodb::Api.adapter.class).to eq(Dynamodb::Api::Adapter)
+    end
   end
 end
