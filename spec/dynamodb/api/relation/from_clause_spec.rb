a7e8549ca1c6bec6144147a30ec06d19a98480f7
WalkerSumida <walkersumida@gmail.com>
2018-08-12 14:48:48 +0900

    test: from clause spec

---
+++ spec/dynamodb/api/relation/from_clause_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Dynamodb::Api::Relation::FromClause do
+  describe '#initialize' do
+    context 'table_name_prefix is empty' do
+      before do
+        Dynamodb::Api::Config.table_name_prefix = ''
+      end
+
+      let(:table_name) { 'table_name' }
+
+      it 'returns a table name' do
+        expect(Dynamodb::Api::Relation::FromClause.new(table_name).name).to eq table_name
+      end
+    end
+
+    context 'table_name_prefix is not empty' do
+      before do
+        Dynamodb::Api::Config.table_name_prefix = 'prefix_'
+      end
+
+      let(:table_name) { 'table_name' }
+
+      it 'returns a table name' do
+        expect(Dynamodb::Api::Relation::FromClause.new(table_name).name).to eq('prefix_' + table_name)
+      end
+    end
+  end
+end
+
