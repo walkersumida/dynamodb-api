a1e7d5c84b64b29b2711ff9d49acf4fb8a24ca93
WalkerSumida <walkersumida@gmail.com>
2018-08-12 14:47:49 +0900

    refact: adapter spec

--- spec/dynamodb/api/adapter_spec.rb
+++ spec/dynamodb/api/adapter_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe Dynamodb::Api::Adapter do
-  it 'has a aws dynamodb client' do
-    expect(Dynamodb::Api::Adapter.new.client.class).to be Aws::DynamoDB::Client
+  describe '#initialize' do
+    it 'has a aws dynamodb client' do
+      expect(Dynamodb::Api::Adapter.new.client.class).to be Aws::DynamoDB::Client
+    end
   end
 end
 
