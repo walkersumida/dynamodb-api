a478eab5675321beb56f99fa5b73935571b9deaa
WalkerSumida <walkersumida@gmail.com>
2018-09-24 00:38:34 +0900

    test: change reserved words spec

--- spec/dynamodb/api/reserved_words_spec.rb
+++ spec/dynamodb/api/reserved_words_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Dynamodb::Api::ReservedWords do
   describe '#all' do
     it 'works' do
-      expect(Dynamodb::Api::ReservedWords.all.count).to eq(573)
+      expect(Dynamodb::Api::ReservedWords.new.all.count).to eq(573)
     end
   end
 end
