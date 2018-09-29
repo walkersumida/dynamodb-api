62c837be2215b6d2e09371e112245f40f3ae0ea2
WalkerSumida <walkersumida@gmail.com>
2018-09-29 18:16:49 +0900

    test: add expression attribute names and filter spec

--- spec/dynamodb/api/relation/filter_clause_spec.rb
+++ spec/dynamodb/api/relation/filter_clause_spec.rb
@@ -2,11 +2,29 @@
 
 RSpec.describe Dynamodb::Api::Relation::FilterClause do
   describe '#initialize' do
-    it 'returns filter expression and values' do
-      filter_clause = Dynamodb::Api::Relation::FilterClause.
-        new('model = :model', ':model': 'S2000')
-      expect(filter_clause.expression).to eq('model = :model')
-      expect(filter_clause.values).to eq(':model': 'S2000')
+    context 'when has not reserved words' do
+      it 'returns filter expression and values' do
+        filter_clause = Dynamodb::Api::Relation::FilterClause.
+          new('model = :model', ':model': 'S2000')
+
+        expect(filter_clause.expression).to eq('model = :model')
+        expect(filter_clause.values).to eq(':model': 'S2000')
+        expect(filter_clause.reserved_words).to eq([])
+      end
+    end
+
+    context 'when has reserved words' do
+      it 'returns filter expression and values' do
+        filter_clause = Dynamodb::Api::Relation::FilterClause.
+          new('model = :model and #status = :status and #year = :year',
+              ':model': 'S2000', '#status': 1, '#year': 2018)
+
+        expect(filter_clause.expression).
+          to eq('model = :model and #status = :status and #year = :year')
+        expect(filter_clause.values).
+          to eq(':model': 'S2000', '#status': 1, '#year': 2018)
+        expect(filter_clause.reserved_words).to eq(['#status', '#year'])
+      end
     end
   end
 end
