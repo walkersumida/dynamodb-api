b29a635f8ec6ebbd0c859acaab3da2b11c8a39e0
WalkerSumida <walkersumida@gmail.com>
2018-10-21 14:55:53 +0900

    test: add Api#delete

--- spec/dynamodb/api_spec.rb
+++ spec/dynamodb/api_spec.rb
@@ -58,4 +58,33 @@ RSpec.describe Dynamodb::Api do
       expect(result['model']).to eq('NSX')
     end
   end
+
+  describe '#delete' do
+    subject { Dynamodb::Api.delete(table_name, item) }
+
+    before do
+      items = [
+        { maker_id: 1, maker: 'Honda', model: 'Accord', release_date: 19760508, status: 0 },
+        { maker_id: 1, maker: 'Honda', model: 'S2000', release_date: 19980101, status: 1 },
+      ]
+      DynamodbHelper.new.create_dummy_data(items)
+    end
+
+    let(:table_name) { 'cars' }
+    let(:item) do
+      { model: 'S2000', release_date: 19980101 }
+    end
+    let(:results) do
+      query = Dynamodb::Api.query
+      query.from('cars').index('index_maker_id_release_date')
+      query.where(['maker_id', 'EQ', 1])
+      query.all.items
+    end
+
+    it 'works' do
+      is_expected
+      expect(results.detect { |i| i['model'] == 'S2000' }).to eq(nil)
+      expect(results.count).to eq(1)
+    end
+  end
 end
