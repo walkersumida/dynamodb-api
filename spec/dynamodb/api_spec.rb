a3989f9319dd0db880f040f3375c37c8d430078c
WalkerSumida <walkersumida@gmail.com>
2018-10-28 16:55:41 +0900

    test: add update spec

--- spec/dynamodb/api_spec.rb
+++ spec/dynamodb/api_spec.rb
@@ -59,6 +59,41 @@ RSpec.describe Dynamodb::Api do
     end
   end
 
+  describe '#update' do
+    subject { Dynamodb::Api.update(table_name, key, item) }
+
+    before do
+      DynamodbHelper.new.create_dummy_data(items)
+    end
+
+    let(:items) do
+      [
+        { maker_id: 1, maker: 'Honda', model: 'Accord', release_date: 19760508, status: 0 },
+        { maker_id: 1, maker: 'Honda', model: 'S2000', release_date: 19980101, status: 1 },
+      ]
+    end
+    let(:table_name) { 'cars' }
+    let(:key) do
+      { model: 'Accord', release_date: 19760508 }
+    end
+    let(:item) do
+      { status: 1, new_col: 'new' }
+    end
+    let(:results) do
+      query = Dynamodb::Api.query
+      query.from('cars').index('index_maker_id_release_date')
+      query.where(['maker_id', 'EQ', 1]).filter('#status = :status', ':status': 1)
+      query.all.items
+    end
+
+    it 'works' do
+      is_expected
+      expect(results.detect { |i| i['model'] == 'Accord' }['model']).to eq(items[0][:model])
+      expect(results.detect { |i| i['model'] == 'Accord' }['new_col']).to eq('new')
+      expect(results.count).to eq(2)
+    end
+  end
+
   describe '#delete' do
     subject { Dynamodb::Api.delete(table_name, item) }
 
