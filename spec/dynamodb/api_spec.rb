f4e41c6a641a8fbb0fea4b5c6bbfccfa4b528591
WalkerSumida <walkersumida@gmail.com>
2018-12-02 16:21:09 +0900

    test: add Dynamodb::Api.remove_attributes spec

--- spec/dynamodb/api_spec.rb
+++ spec/dynamodb/api_spec.rb
@@ -130,4 +130,41 @@ RSpec.describe Dynamodb::Api do
       expect(results.count).to eq(1)
     end
   end
+
+  describe '#remove_attributes' do
+    subject { Dynamodb::Api.remove_attributes(table_name, key, attributes) }
+
+    before do
+      DynamodbHelper.new.create_dummy_data(items)
+    end
+
+    let(:items) do
+      [
+        {
+          id: '1', maker_id: 1, maker: 'Honda', model: 'Accord', release_date: 19760508, status: 1,
+        },
+        {
+          id: '4', maker_id: 1, maker: 'Honda', model: 'S2000', release_date: 19980101, status: 1,
+        },
+      ]
+    end
+    let(:table_name) { 'cars' }
+    let(:key) do
+      { id: '1' }
+    end
+    let(:attributes) do
+      ['status']
+    end
+    let(:results) do
+      query = Dynamodb::Api.query
+      query.from('cars').index('index_maker_id_release_date')
+      query.where(['maker_id', 'EQ', 1]).filter('#status = :status', ':status': 1)
+      query.all.items
+    end
+
+    it 'works' do
+      is_expected
+      expect(results.count).to eq(1)
+    end
+  end
 end
