06b835631e88f9f819cfe0da1ab9f331b2dc01ed
WalkerSumida <walkersumida@gmail.com>
2019-06-01 21:36:30 +0900

    refact: move before/let

--- spec/dynamodb/api/delete/item_spec.rb
+++ spec/dynamodb/api/delete/item_spec.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe Dynamodb::Api::Delete::Item do
+  before do
+    items = [
+      {
+        id: '1', maker_id: 1, maker: 'Honda', model: 'Accord', release_date: 19760508, status: 0,
+      },
+      {
+        id: '4', maker_id: 1, maker: 'Honda', model: 'S2000', release_date: 19980101, status: 1,
+      },
+    ]
+    DynamodbHelper.new.create_dummy_data(items)
+  end
+
+  let(:table_name) { 'cars' }
+  let(:item) do
+    { id: '4' }
+  end
+
   describe '#delete_item' do
     subject { Dynamodb::Api::Delete::Item.delete_item(item, table_name) }
 
-    before do
-      items = [
-        {
-          id: '1', maker_id: 1, maker: 'Honda', model: 'Accord', release_date: 19760508, status: 0,
-        },
-        {
-          id: '4', maker_id: 1, maker: 'Honda', model: 'S2000', release_date: 19980101, status: 1,
-        },
-      ]
-      DynamodbHelper.new.create_dummy_data(items)
-    end
-
-    let(:table_name) { 'cars' }
-    let(:item) do
-      { id: '4' }
-    end
     let(:results) do
       query = Dynamodb::Api.query
       query.from('cars').index('index_maker_id_release_date')
