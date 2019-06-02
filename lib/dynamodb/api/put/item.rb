db9a1846c2202cc603a48109a6a7a6a510d84e97
WalkerSumida <walkersumida@gmail.com>
2019-06-02 23:36:44 +0900

    fix: not be added table name prefix

--- lib/dynamodb/api/put/item.rb
+++ lib/dynamodb/api/put/item.rb
@@ -3,9 +3,13 @@
 module Dynamodb
   module Api
     module Put
-      class Item # :nodoc:
+      class Item
+        # @param key [Hash] the item key
+        # @param table_name [String] the table name
         def self.put_item(item, table_name)
           client = Adapter.client
+          table_name = Dynamodb::Api::Config.build_table_name(table_name)
+
           client.put_item(item: item, table_name: table_name)
         end
       end
