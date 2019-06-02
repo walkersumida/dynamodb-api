9dc62619b1d6215ff8eb63bf77c2d7cc3bb10abc
WalkerSumida <walkersumida@gmail.com>
2019-06-02 23:31:44 +0900

    refact: move before/let(Put::Item class)

--- spec/dynamodb/api/put/item_spec.rb
+++ spec/dynamodb/api/put/item_spec.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe Dynamodb::Api::Put::Item do
+  before do
+    DynamodbHelper.new.create_dummy_data
+  end
+
+  let(:table_name) { 'cars' }
+  let(:item) do
+    { id: '5', maker_id: 1, maker: 'Honda', model: 'NSX', release_date: 19900914 }
+  end
+
   describe '#put_item' do
     subject { Dynamodb::Api::Put::Item.put_item(item, table_name) }
 
-    before do
-      DynamodbHelper.new.create_dummy_data
-    end
-
-    let(:table_name) { 'cars' }
-    let(:item) do
-      { id: '5', maker_id: 1, maker: 'Honda', model: 'NSX', release_date: 19900914 }
-    end
     let(:result) do
       query = Dynamodb::Api.query
       query.from('cars').index('index_maker_id_release_date')
