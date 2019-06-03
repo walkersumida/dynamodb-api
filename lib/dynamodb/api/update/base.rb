4bdb44f86fc4802eb8e5233719ead3e561789046
WalkerSumida <walkersumida@gmail.com>
2019-06-03 23:28:33 +0900

    fix: not be added table name prefix

--- lib/dynamodb/api/update/base.rb
+++ lib/dynamodb/api/update/base.rb
@@ -7,7 +7,7 @@ module Dynamodb
         def update_item(key, cols, table_name)
           @key = key
           @cols = cols
-          @table_name = table_name
+          @table_name = Dynamodb::Api::Config.build_table_name(table_name)
 
           client = Adapter.client
           client.update_item(build_update_clause)
